efca7d8cb6fe65b7a3021875d5ac70472c748c72
muhd ameen <ameens.in19@gamil.com>
2022-02-19 23:58:56 +0530

    appconstands change

--- lib/Commons/AppConstants.dart
+++ lib/Commons/AppConstants.dart
@@ -1,4 +1,5 @@
-const String googleAPI = "AIzaSyAJo7ZMx1xhvdFX4xZCWoz8RgJ4f-NOtHc";
+// const String googleAPI = "AIzaSyAJo7ZMx1xhvdFX4xZCWoz8RgJ4f-NOtHc";
+const String googleAPI = "AIzaSyCawA2mYM2T99o2vyHbdFMlGggS4qNq1IA";
 // const String baseUrl = "192.168.29.133:5000";
 // const String socketUrl = "http://192.168.29.138:5000/";
 const String baseUrl = "ausmart.live";
